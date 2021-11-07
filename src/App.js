30b7d50ff31b8dff0f79ef1a8ee3a7184a123396
Muhammad Ibn Saeed <mi.saeed@yahoo.com>
2021-11-08 00:04:01 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FetchingPost from './Components/FetchingPost';
+import PostForm from './Components/PostForm';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <FetchingPost /> */}
+      <PostForm />
+    </div>
+  );
+}
+
+export default App;
